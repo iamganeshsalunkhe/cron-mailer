9549ea9ec6ff252c0f48c8aa15f18db648b1cb2d
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-09-02 10:08:45 +0530

    install dependencies and server test

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+// import the required modules
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// configuration of dotenv
+dotenv.config();
+
+// initiate port
+const PORT = process.env.PORT || 4200;
+
+// initiate app
+const app = express();
+
+// using middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credential:true
+}
+));
+
+// listening
+app.listen(PORT,()=>{
+    console.log(`Server listening on PORT ${PORT}`);
+});

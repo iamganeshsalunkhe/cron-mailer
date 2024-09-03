a6816618040bb87e20859ec8e9eccbd6d2b9e99e
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-09-03 14:50:19 +0530

    forgot-password page add

--- Frontend/src/Components/Forgotpassword.jsx
+++ Frontend/src/Components/Forgotpassword.jsx
@@ -1,17 +1,22 @@
-import { Link } from "react-router-dom";
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+function Forgotpassword() {
+    const navigate = useNavigate();
+
   // state for input  email, password
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword,setConfirmPassword]= useState('');
 
   // state for toggle password visibility
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
+  // state for toggle confirm password visibility
+  const [isConfirmPasswordVisible,setIsConfirmPasswordVisible]= useState(false);
 
   // check user filled all required fields
   const [isFormValid, setIsFormValid] = useState(false);
@@ -19,16 +24,20 @@ function Login() {
   // run the effect when some changes on UI
   useEffect(() => {
     setIsFormValid(
-        email !== "" &&
-        password !== ""
+        email !== "" && 
+        password !== "" &&
+        confirmPassword !== '' && 
+        password === confirmPassword
     );
-  }, [ email, password]);
+  }, [email, password,confirmPassword]);
 
   const handlePasswordVisibility = () => {
     setIsPasswordVisible((prevState) => !prevState);
   };
 
-  
+  const handleConfirmPasswordVisibility = ()=>{
+    setIsConfirmPasswordVisible(prevState =>!prevState);
+  }
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -37,24 +46,25 @@ function Login() {
       password,
     };
     await axios
-      .post("http://localhost:4200/login", userData)
+      .post("http://localhost:4200/forgotpassword", userData)
       .then((res) => {
         if (res.data) {
-          toast.success("LoggedIn  successfully");
+          toast.success("Password changed  successfully");
+          navigate('/login');
         }
       })
       .catch((err) => {
         if (err.response) {
-          toast.error("Their is problem while creating logging in!!");
+          toast.error("Their is problem while setting a new password!!");
         }
       });
   };
   return (
     <div className="flex items-center justify-center min-h-screen bg-indigo-500">
-      <div className="w-72 max-w-md p-2 rounded-xl shadow-md  bg-white h-[330px]">
+      <div className="w-72 max-w-md p-2 rounded-xl shadow-md  bg-white h-[340px]">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Log In
+            Forgot Password
           </h2>
         </div>
 
@@ -74,7 +84,8 @@ function Login() {
                 />
               </div>
             </div>
-
+            
+            {/* for getting password */}
             <div>
               <div className="mt-2 relative">
                 <input
@@ -85,7 +96,7 @@ function Login() {
                   onChange={(e) => setPassword(e.target.value)}
                   autoComplete="current-password"
                   className="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
-                  placeholder="Enter your password"
+                  placeholder="create a new password"
                 />
                 <div
                   className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
@@ -94,13 +105,27 @@ function Login() {
                   {isPasswordVisible ? <IoMdEyeOff /> : <IoMdEye />}
                 </div>
               </div>
-              <div className="text-sm flex justify-end">
-                <Link
-                  to='/forgotpassword'
-                  className="font-semibold text-indigo-600 hover:text-indigo-500 mt-2 "
+            </div>
+
+            {/* for confirm password */}
+            <div>
+              <div className="mt-2 relative">
+                <input
+                  id="password"
+                  name="password"
+                  type={isConfirmPasswordVisible ? "text" : "password"}
+                  required
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  autoComplete="current-password"
+                  className="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                  placeholder="Confirm a new password"
+                />
+                <div
+                  className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={handleConfirmPasswordVisibility}
                 >
-                  Forgotten password?
-                </Link>
+                  {isPasswordVisible ? <IoMdEyeOff /> : <IoMdEye />}
+                </div>
               </div>
             </div>
 
@@ -111,24 +136,14 @@ function Login() {
                       cursor-pointer"
                 disabled={!isFormValid}
               >
-                Log In
+                Set new password
               </button>
             </div>
           </form>
-
-          <p className="mt-5 text-center text-sm text-gray-500">
-            Don&apos;t have an account?
-            <Link
-              to="/signup"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Register
-            </Link>
-          </p>
         </div>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default Forgotpassword;
